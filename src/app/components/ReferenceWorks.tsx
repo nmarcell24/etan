bb3769a1252ed40c477f3519c9b90c68a565680a
nmarci <nemethmarcell1224@gmail.com>
2026-01-05 15:31:08 +0100

    ...

--- src/app/components/ReferenceWorks.tsx
+++ src/app/components/ReferenceWorks.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from 'framer-motion';
-import { Home, Building2, ArrowRight } from "lucide-react";
+import { Home } from "lucide-react";
 import Image from 'next/image';
 
 const works = [
